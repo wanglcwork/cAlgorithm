ec7f563f9d676e73c829a5c747e09f4252231b67
wanglc_work <wanglc_work@163.com>
2017-06-09 10:55:25 +0800

    bubble 2 和 3 参数传错了

--- CPlusPlusAlgorithm/CPlusPlusAlgorithm/CPlusPlusAlgorithm.cpp
+++ CPlusPlusAlgorithm/CPlusPlusAlgorithm/CPlusPlusAlgorithm.cpp
@@ -11,17 +11,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	//test,c++的初始化,对象或者是new 指针,java中不能这样初始化.
 	/*SortBubble sb;*/ // 静态方法可以用对象.操作符调用,也可以用指针的箭头操作符调用.
 
-	int arr1[5] = { 1, 3, 4, 2, 5 };
+	const int N = 5;
+
+	int arr1[N] = { 1, 3, 4, 2, 5 }; // N必须是const常量
 	SortBubble::Bubble1(arr1, 5);
 	SortUtil::printArray(arr1, 5);
 
-	int arr2[5] = { 1, 3, 4, 2, 5 };
-	SortBubble::Bubble2(arr1, 5);
-	SortUtil::printArray(arr1, 5);
+	int arr2[N] = { 1, 3, 4, 2, 5 };
+	SortBubble::Bubble2(arr2, 5);
+	SortUtil::printArray(arr2, 5);
 
-	int arr3[5] = { 1, 4, 5, 2, 3 };
-	SortBubble::Bubble3(arr1, 5);
-	SortUtil::printArray(arr1, 5);
+	int arr3[N] = { 1, 4, 5, 2, 3 };
+	SortBubble::Bubble3(arr3, 5);
+	SortUtil::printArray(arr3, 5);
 
 
 	system("pause"); // 需要包含 #include <iostream>
