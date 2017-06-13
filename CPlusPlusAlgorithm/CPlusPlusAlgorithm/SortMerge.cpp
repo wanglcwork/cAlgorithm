9b974e9097e17fffaf722f5fae5943d208aa84ad
wanglc_work <wanglc_work@163.com>
2017-06-13 16:48:11 +0800

    bug

--- CPlusPlusAlgorithm/CPlusPlusAlgorithm/SortMerge.cpp
+++ CPlusPlusAlgorithm/CPlusPlusAlgorithm/SortMerge.cpp
@@ -17,30 +17,32 @@ void SortMerge::mergeSort(int a[], int n) {
 	{
 		return;
 	}
-	mergeSort1(a, 0, n-1, temp);
+	mergeSort1(a, 0, n-1, temp); // first,last 要排序的第一个和最后一个元素的下标.
 }
 
 void SortMerge::mergeSort1(int a[], int fist, int last, int temp[]) {
 
-	if (fist < last)
-	{
-		int mid = (fist + last) / 2;
-		mergeSort1(a, fist, mid, temp);
-		mergeSort1(a, mid + 1, last, temp);
-		mergeArray(a, fist, mid, last, temp);
+	if (fist >= last) {
+		return;
 	}
+
+	int mid = (fist + last) / 2;
+	mergeSort1(a, fist, mid, temp); // 左边排序
+	mergeSort1(a, mid + 1, last, temp); // 右边排序
+	mergeArray(a, fist, mid, last, temp); // 合并两边的数组
+	
 }
 
 void SortMerge::mergeArray(int a[], int first, int mid, int last, int temp[]) {
 	int i = first;
-	int j = mid + 1;
+	int j = mid + 1; // 右边数组的第一个元素.
 	int k = 0;
 
 	while (i <= mid && j <= last)
 	{
 		if (a[i] < a[j])
 		{
-			temp[k++] = a[i++];
+			temp[k++] = a[i++]; // i++只能这么写了.问题不大.
 		}
 		else
 		{
@@ -53,14 +55,14 @@ void SortMerge::mergeArray(int a[], int first, int mid, int last, int temp[]) {
 		temp[k++] = a[i++];
 	}
 
-	while (j<last)
+	while (j<=last)
 	{
-		temp[k++] = a[i++];
+		temp[k++] = a[j++];
 	}
 
 	for (int i = 0; i < k; i++)
 	{
-		a[first+i] = temp[i];
+		a[first+i] = temp[i]; // 从first元素开始排序,包含first
 	}
 }
 
